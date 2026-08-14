3f709aedf583806096de7001ebe3fc985c19b5ab
wupengbo <wupengbo125@qq.com>
2026-08-15 01:14:29 +0800

    update

--- install-others.sh
+++ install-others.sh
@@ -6,6 +6,7 @@ COMMANDS=(
   "npx -y skills@latest add Agents365-ai/365-skills"
   "npx skills add OthmanAdi/planning-with-files --skill planning-with-files "
   "curl -fsSL https://pi.dev/install.sh | sh"
+  "npx skills add https://github.com/juliusbrussee/caveman"
 )
 
 # 初始化选中状态（0=未选, 1=已选）
